f69dd74fc288fa249c2b3d9a7a1feb25d04ee577
Yves Dubugnon <yves.dubugnon@gmail.com>
2015-03-05 01:16:19 +0100

    Displaying Cosine function as designed

--- src/main/scala/model/function/MathematicalFunctions.scala
+++ src/main/scala/model/function/MathematicalFunctions.scala
@@ -23,7 +23,7 @@ class MathematicalFunctions(mathematicalFunctions: MathematicalFunction*) {
 trait MathematicalFunction {
   val description: String
 
-  def run(x: String): String
+  def run(x: Double): Double
 
   override def toString = description
 }
@@ -31,50 +31,36 @@ trait MathematicalFunction {
 object Sine extends MathematicalFunction {
   val description: String = "Sine"
 
-  def run(x: String): String = try {
-    (0.5 + Math.sin(x.toDouble * 4 * Math.PI) / 2).toString
-  } catch {
-    case ex: Throwable => ex.toString
-  }
+  def run(x: Double): Double = (0.5 + Math.sin(x * 4 * Math.PI) / 2)
 }
 
 object Cosine extends MathematicalFunction {
   val description: String = "Cosine"
 
-  def run(x: String): String = try {
-    (0.5 + Math.cos(x.toDouble * Math.PI) / 2).toString
-  } catch {
-    case ex: Throwable => ex.toString
-  }
+  def run(x: Double): Double = (0.5 + Math.cos(x * Math.PI) / 2)
 }
 
 object Complex extends MathematicalFunction {
   val description: String = "Complex"
 
-  def run(x: String): String = try {
-    (0.5 + (Math.sin(x.toDouble * 3 * Math.PI) + Math.sin(x.toDouble * 7 * Math.PI) + Math.sin(x.toDouble * 8 * Math.PI) + Math.sin(x.toDouble * 11 * Math.PI)) / 8).toString
-  } catch {
-    case ex: Throwable => ex.toString
-  }
+  def run(x: Double): Double = 0.5 + (Math.sin(x * 3 * Math.PI) + Math.sin(x * 7 * Math.PI) + Math.sin(x * 8 * Math.PI) + Math.sin(x * 11 * Math.PI)) / 8
 }
 
 object Chaos extends MathematicalFunction {
   val description: String = "Chaos"
   var Ikeda: Array[Double] = initIkeda()
 
-  def run(x: String): String = try {
+  def run(x: Double): Double = {
     var value: Double = 0
     var i: Int = 0
     var dx: Double = .0
-    if (x.toDouble >= 2.00) value = Ikeda(200)
+    if (x >= 2.00) value = Ikeda(200)
     else {
-      i = (x.toDouble * 100.0).toInt
-      dx = 100 * x.toDouble - i.toDouble
+      i = (x * 100.0).toInt
+      dx = 100 * x - i
       value = Ikeda(i) * (1 - dx) + Ikeda(i + 1) * dx
     }
-    value.toString
-  } catch {
-    case ex: Throwable => ex.toString
+    value
   }
 
   def initIkeda(): Array[Double] = {
